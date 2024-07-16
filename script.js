4934c2f6c9391eb77e2f2c8013a7dc2bac29f5c4
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-07-15 20:58:57 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const btn = document.querySelector("[data-form-btn]");
-const input = document.querySelector("[data-form-input]");
 
 // Funcion para crear una tarea
 const createTask = (event) => {
   event.preventDefault();
-  console.log("Se le dio click al boton");
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+
+  // Agregando los estilos a la card
+  task.classList.add("card");
+
+  input.value = "";
+  // Almacenando value las etiquetas html respectivas
+  const content = `
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>
+  `;
+  // Agregandolo al HTML
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+  console.log(value);
+  console.log(content);
 };
 
 // Evento de escucha
