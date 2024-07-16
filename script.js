3991dca08385cb8e855c01dad14a1ff735d2732b
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-07-16 10:10:17 -0600

    clase04 completada

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
-const btn = document.querySelector("[data-form-btn]");
+// Funcion que en cuanto se declara se ejecuta
+// IIFE
+// Immediately invoked function expression IIFE
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
 
-// Funcion para crear una tarea
-const createTask = (event) => {
-  event.preventDefault();
-  const input = document.querySelector("[data-form-input]");
-  const value = input.value;
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
+  // Funcion para crear una tarea
+  const createTask = (event) => {
+    event.preventDefault();
+    // Input
+    const input = document.querySelector("[data-form-input]");
+    // Capturando valor del input
+    const value = input.value;
+    // Valor a 0 en el input
+    input.value = "";
 
-  // Agregando los estilos a la card
-  task.classList.add("card");
+    // Lista
+    const list = document.querySelector("[data-list]");
 
-  input.value = "";
-  // Almacenando value las etiquetas html respectivas
-  const content = `
-    <div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>
-  `;
-  // Agregandolo al HTML
-  task.innerHTML = content;
+    // Tarea
+    const task = document.createElement("li");
+    // Agregando los estilos a la card de la tarea
+    task.classList.add("card");
 
-  list.appendChild(task);
+    // Contenido de la card "CONTENEDOR"
+    const taskContent = document.createElement("div");
 
-  console.log(value);
-  console.log(content);
-};
+    // Agregando clase para los estilos del contenedor de la card
+    taskContent.classList.add("content-card");
 
-// Evento de escucha
-btn.addEventListener("click", createTask);
+    // Contenido de la card "CHECK BUTTON"
+    taskContent.appendChild(createCheckButton());
+    // Contenido de la card "TEXTO"
+    taskContent.appendChild(createTextTask(value));
+
+    // Agregando boton de eliminado de tarea
+    taskContent.appendChild(createDropButton());
+
+    // Agregando el contenido de la tarea
+    task.appendChild(taskContent);
+
+    // Agregando la tarjeta a la lista
+    list.appendChild(task);
+  };
+
+  // Evento de escucha
+  btn.addEventListener("click", createTask);
+
+  const createCheckButton = () => {
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const createTextTask = (value) => {
+    const textContentTask = document.createElement("span");
+    textContentTask.classList.add("task");
+    textContentTask.innerText = value;
+    return textContentTask;
+  };
+
+  const createDropButton = () => {
+    const taskDropButton = document.createElement("i");
+    taskDropButton.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    return taskDropButton;
+  };
+
+  const completeTask = (e) => {
+    const element = e.target;
+    element.classList.toggle("far");
+    element.classList.toggle("fas");
+    element.classList.toggle("complete-icon");
+  };
+})();
